2983c0adfd4ee0b1f00c276a338ab8adefa5fa1a
Justin Soliz <justinsoliz@gmail.com>
2011-09-16 11:22:33 -0500

    updating rabbitmq script to 2.6.1

--- install-rabbitmq.sh
+++ install-rabbitmq.sh
@@ -1,33 +1,32 @@
+mkdir src
 cd /src
 mkdir rabbitmq
 cd rabbitmq
 
 sudo apt-get install erlang-nox
-sudo apt-get -fy install
 
-sudo wget http://www.rabbitmq.com/releases/rabbitmq-server/v2.5.1/rabbitmq-server_2.5.1-1_all.deb
+# not sure what this does
+# sudo apt-get -fy install
 
-sudo dpkg -i rabbitmq-server_2.5.1-1_all.deb
-<<<<<<< HEAD
-sudo /etc/init.d/rabbitmq-install stop
+sudo wget http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1/rabbitmq-server_2.6.1-1_all.deb
 
-cd /usr/lib/rabbitmq/lib/rabbitmq_server-2.5.1/plugins
+sudo dpkg -i rabbitmq-server_2.6.1-1_all.deb
 
-
-
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/mochiweb-2.5.1.ez
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/webmachine-2.5.1.ez
-=======
 sudo rabbitmqctl stop
 
 cd /usr/lib/rabbitmq/lib/rabbitmq_server-2.5.1/plugins
 
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/mochiweb-1.3-rmq2.5.1-git9a53dbd.ez
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/webmachine-1.7.0-rmq2.5.1-hg0c4b60a.ez
->>>>>>> 708422280ced8937e4f55288ed99d4b969813404
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/amqp_client-2.5.1.ez
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/rabbitmq_mochiweb-2.5.1.ez
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/rabbitmq_management_agent-2.5.1.ez
-sudo wget http://www.rabbitmq.com/releases/plugins/v2.5.1/rabbitmq_management-2.5.1.ez
+sudo wget http://www.rabbitmq.com/releases/plugins/v2.6.1/mochiweb-1.3-rmq2.6.1-git9a53dbd.ez
 
-sudo /etc/init.d/rabbitmq-server start
+sudo wget http://www.rabbitmq.com/releases/plugins/v2.6.1/webmachine-1.7.0-rmq2.6.1-hg0c4b60a.ez
+
+sudo wget http://www.rabbitmq.com/releases/plugins/v2.6.1/amqp_client-2.6.1.ez
+
+sudo wget http://www.rabbitmq.com/releases/plugins/v2.6.1/rabbitmq_mochiweb-2.6.1.ez
+
+sudo wget http://www.rabbitmq.com/releases/plugins/v2.6.1/rabbitmq_management_agent-2.6.1.ez
+
+sudo wget http://www.rabbitmq.com/releases/plugins/v2.6.1/rabbitmq_management-2.6.1.ez
+
+# stop/start/etc..
+sudo invoke-rc.d rabbitmq-server start

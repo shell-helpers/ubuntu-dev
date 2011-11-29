aacfb667de36a5589bbcd7f9700a7132a508dbd4
Alex Robson <asrobson@gmail.com>
2011-11-29 10:14:00 -0600

    Adding curl to the init script

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ CURRENT_USER="$(whoami)"
 sudo chown $CURRENT_USER /src
 echo 'PATH=$PATH:./' >> ~/.bashrc
 source ~/.bashrc
-sudo apt-get -y install build-essential m4 libncurses5-dev openssl libssl-dev libssh-dev unixodbc-dev libgmp3-dev libwxgtk2.8-dev libglu1-mesa-dev fop xsltproc default-jdk
+sudo apt-get -y install curl build-essential m4 libncurses5-dev openssl libssl-dev libssh-dev unixodbc-dev libgmp3-dev libwxgtk2.8-dev libglu1-mesa-dev fop xsltproc default-jdk
